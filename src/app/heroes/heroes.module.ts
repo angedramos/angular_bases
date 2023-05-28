caafde310010bcc21c567b89c32f993bbfafa45f
Angel Eduardo Ramos Mena <angedramos@gmail.com>
2023-05-27 21:06:47 -0500

    Comandos nuevos de git

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,7 +4,8 @@ import { CommonModule } from "@angular/common";
 import { HeroComponent } from "./hero/hero.component";
 import { ListComponent } from "./list/list.component";
 
-
+//git checkout -- . -> regresa a la version del proyecto que estaba antes del ultimo commit
+//git branch -M main -> renombre la rama en la que estamos
 
 @NgModule({
     declarations:[
